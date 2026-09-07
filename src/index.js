b0077cc3bbada3f5fe3aa4b71f670d6da47b71ba
ABHINAV-SHUKLA-creator <vivobook016@gmail.com>
2026-09-07 17:15:47 +0530

    db connection

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js"
+
+
+// ;(async () => {
+//     try{
+//         await mongoose.connect(process.env.mongoURI, {
+//             dbName: DB_NAME,
+//         });
+//         app.on("error", (err) => {
+//             console.log("error:", err);
+//             throw err;
+//         })
+
+//         app.listen(process.env.port, () => {
+//             console.log(`server is running on port ${process.env.port}`);
+//         });
+//     }catch(err){
+//         console.log("error:",err)
+//         throw err
+//     }
+// })()
